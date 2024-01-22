cdefcfaa1a71146aa7b767714c7014395c514937
이세원 <sewon@isewon-ui-noteubug.local>
2024-01-22 20:43:24 +0900

    chore : add basic setup code

---
+++ src/main/java/UMC/campusNote/user/User.java
@@ -0,0 +1,27 @@
+package UMC.campusNote.user;
+
+import UMC.campusNote.mapping.UserLesson;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "USER_ID")
+    private Long id;
+
+    @OneToMany(mappedBy = "member")
+    @Builder.Default
+    List<UserLesson> userLessonList = new ArrayList<>();
+}
