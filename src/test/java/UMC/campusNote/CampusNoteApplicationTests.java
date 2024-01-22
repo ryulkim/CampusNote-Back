6beca33ff12d30836e3ee27782ba824dd463e124
이세원 <sewon@isewon-ui-noteubug.local>
2024-01-22 19:54:38 +0900

    first commit

---
+++ src/test/java/UMC/campusNote/CampusNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package UMC.campusNote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
