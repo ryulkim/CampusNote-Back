0dcb0150a1d40b6104b36da9e077ebc2eb17df41
Gyuhyeok <ghkdrbgur13@naver.com>
2024-01-23 17:01:24 +0900

    #4 feat : Api 응답 및 공통 예외 처리 기능 추가

---
+++ src/main/java/UMC/campusNote/common/code/BaseCode.java
@@ -0,0 +1,8 @@
+package UMC.campusNote.common.code;
+
+public interface BaseCode {
+
+    public ReasonDTO getReason();
+
+    public ReasonDTO getReasonHttpStatus();
+}
---
+++ src/main/java/UMC/campusNote/common/code/ErrorReasonDTO.java
@@ -0,0 +1,17 @@
+package UMC.campusNote.common.code;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Builder
+public class ErrorReasonDTO {
+
+    private HttpStatus httpStatus;
+
+    private final boolean isSuccess;
+    private final String code;
+    private final String message;
+
+}
